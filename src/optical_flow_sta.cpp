250bed0d4a671f40c066b68b9f9dc4f2ad8c1c98
Srđan Rašić <srdan.rasic@gmail.com>
2013-05-23 17:09:41 +0200

    Initial project setup.

---
+++ src/optical_flow_sta.cpp
@@ -0,0 +1,14 @@
+//
+//  optical_flow_sta.cpp
+//  optical_flow_sta
+//
+//  Created by Srđan Rašić on 5/23/13.
+//  Copyright (c) 2013 Srđan Rašić. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int, char**)
+{
+  return 0;
+}
